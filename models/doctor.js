f95d2aa9a1f10f5d4977195d5c45f290187899c9
vinhle000 <vinhle000@gmail.com>
2022-02-15 13:49:40 -0800

    Added get all and create for doctors collection

--- models/doctor.js
+++ models/doctor.js
@@ -1,3 +1,16 @@
 const mongoose = require('mongoose')
 
 
+const doctorSchema = new mongoose.Schema({
+  firstName:{
+    type: String,
+    required: true,
+  },
+  lastName:{
+    type: String,
+    require: true,
+  }
+})
+
+
+module.exports = mongoose.model('Doctor', doctorSchema)
--- routes/doctors.js
+++ routes/doctors.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const Doctors = require('../models/doctor')
 
 
-
-//Get all
+// ● Get a list of all doctors
 router.get('/', async (req, res) => {
-  res.send('Hello World')
+
+  try {
+    const doctors = await Doctors.find()
+    res.json(doctors)
+  } catch (err) {
+    res.status(500).json({message: err.message})
+  }
+})
+
+
+//Create doctor record
+router.post('/', async (req, res) => {
+
+  const doctor = new Doctors ({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  })
+  console.log(doctor)
+
+  try {
+    const newDoctor = await doctor.save();
+    res.status(201).json(newDoctor)
+  } catch (err) {
+    res.status(500).json({message: err.message})
+  }
 
 })
+
+
+// ● Delete an existing appointment from a doctor's calendar
+// ● Add a new appointment to a doctor's calendar
+// ○ New appointments can only start at 15 minute intervals (ie, 8:15AM is a valid time but 8:20AM is not)
+
 
 module.exports = router;
